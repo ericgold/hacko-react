ed0251140546927ae34bff3c7f104ff48843c8cb
ericgold <egold05@gmail.com>
2017-07-28 20:04:19 -0700

    login render method

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
-		}
+			username: '',
+			password: '',
+			errors: []
+		};
 	}
 
 	render() {
 		return (
-			<h1>Login</h1>
+			<div className="login">
+				<h1>Login</h1>
+				<form>
+					<label htmlFor="username"><input type="text" id="username"/>UserName</label>
+					<label htmlFor="password"><input type="password" id="password"/>Password</label>
+					<button type="reset">Reset</button>
+					<button type="submit">Submit</button>
+				</form>
+				
+			</div>
+			
 		)
 	}
 }
